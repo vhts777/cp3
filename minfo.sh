9e4103e8adc281f4cc4d3c6c68e84829ad89a507
561813 <rm561813@fiap.com.br>
2025-05-22 11:57:11 -0300

    Terceiro Commit

--- minfo.sh
+++ minfo.sh
@@ -1,1 +1,6 @@
-echo "ola"
+[ "$1" = hostname ] && (hostname; exit; )
+[ "$1" = uptime ] && (uptime; exit; )
+[ "$1" = disk ] && (df -h /; exit; )
+[ "$1" = all ] && echo "$(hostname) | $(uptime -p) | $(df -h / | awk 'NR==2 {print $1, $2, $3, $4, $5, $6}')"
+
+
